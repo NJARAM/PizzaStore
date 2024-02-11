e40d39feca9ddda869e47708b82aae2fefae394e
njaramoses <moses.njara@attain-es.com>
2024-02-11 14:14:59 +0300

    Added pizza store

--- obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f7f2055c315c86a51b3f5a3026952753916698")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
